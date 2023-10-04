c92620ef5ea2eb4fff368698b4a84a98eaf738be
Saisaihub <saima0327@gmail.com>
2023-10-04 09:17:42 +0100

    latest update

--- SynctoGithub.bat
+++ SynctoGithub.bat
@@ -1,9 +1,10 @@
-echo off
+@echo off
 git config --global user.name "Saisaihub"
 git config --global user.email "saima0327@gmail.com"
 git init
 git add .
 git commit -m "latest update"
 git remote set-url origin git@github.com:Saisaihub/ektopas.git
+git pull origin main  
 git push -u origin main
 pause
